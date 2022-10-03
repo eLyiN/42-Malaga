f0b3b3e85dbdb36ae58723d293e883c94f3d81f9
Shelakh <staffultimaunion@gmail.com>
2022-10-03 12:14:27 +0200

    first_word.c

---
+++ 42-Malaga-Exam-Rank-02/Level_1/first_word.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+int	main(int ac, char **av)
+{
+	int	i;
+
+	i = 0;
+	if(ac == 2)
+	{
+		while(av[1][i] == 32 || av[1][i] == 9)
+		i++;
+		while((av[1][i] != 32 && av[1][i] != 9) && av[1][i])
+		{
+			write(1, &av[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+
+}
