903335035e81f7b9054d2ebe010ff3c790f8833c
shelakh portatil <shelakh@portatil.com>
2022-10-16 10:05:57 +0200

    ft_putstr

---
+++ 42-Malaga-Exam-Rank-02/Level_1/ft_putstr/ft_putstr.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void	ft_putstr(char *str){
+	int i;
+
+	i = 0;
+	while(str[i]){
+
+		write(1, &str[i], 1);
+	i++;
+	}
+}
+
+void	main(void)
+{
+	char str[] = "ESTO ES UNA PRUEBA";
+
+	ft_putstr(str);
+}
